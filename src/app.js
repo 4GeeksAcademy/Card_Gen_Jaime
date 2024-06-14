f6bdbabda00d0ff2fec6e12aac5785b684919c5a
mecrophagus <122439515+mecrophagus@users.noreply.github.com>
2024-06-14 05:44:55 +0000

    realizando finales

--- src/app.js
+++ src/app.js
@@ -5,7 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document
+  .getElementById("generateCard")
+  .addEventListener("click", generateRandomCard);
+
+let cardIndex = 0;
+
+function generateRandomCard() {
+  const suits = ["spade", "club", "heart", "diamond"];
+  const suitSymbols = { spade: "♠", club: "♣", heart: "♥", diamond: "♦" };
+  const ranks = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomRank = ranks[Math.floor(Math.random() * ranks.length)];
+  const suitSymbol = suitSymbols[randomSuit];
+
+  const cardContainer = document.getElementById("cardContainer");
+  const newCard = document.createElement("div");
+  newCard.className = `card ${randomSuit}`;
+  newCard.setAttribute("data-rank", randomRank);
+  newCard.setAttribute("data-suit", suitSymbol);
+  newCard.style.transform = `rotate(${cardIndex *
+    15}deg) translateY(${cardIndex * -10}px)`;
+  newCard.style.opacity = 0;
+  newCard.innerHTML = `${suitSymbol}`;
+
+  cardContainer.appendChild(newCard);
+
+  requestAnimationFrame(() => {
+    newCard.style.opacity = 1;
+    newCard.style.transform = `rotate(${cardIndex *
+      15}deg) translateY(${cardIndex * -10}px) translateX(-50%)`;
+  });
+
+  cardIndex++;
+  if (cardIndex >= 10) {
+    cardIndex = 0;
+    cardContainer.innerHTML = "";
+  }
+}
